fc2bbe4e59f1962825212d000767b6b46d581e4d
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-01 01:48:07 +0200

    binary_trees

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,5 +9,30 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
+
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
+
+	if (binary_tree_is_leaf(tree))
+	{
+		if (tree == NULL)
+		{
+			return;
+		}
+
+		if (tree->parent != NULL)
+		{
+			if (tree->parent->left == tree)
+			{
+				tree->parent->left = NULL;
+			}
+			else if (tree->parent->right == tree)
+			{
+				tree->parent->right = NULL;
+			}
+		}
+		free(tree);
+	}
 }
