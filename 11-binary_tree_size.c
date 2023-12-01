48ae76acb0463393ac0fe3ff01c5b9b692a6d4d6
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-02 00:12:28 +0200

    binary_trees

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_size - whatever
+ *
+ * @tree: ..
+ *
+ * Return: ...
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t x, y;
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	x = tree->left ? binary_tree_size(tree->left) : 0;
+	y = tree->right ? binary_tree_size(tree->right) : 0;
+
+	return (x + y + 1);
+}
