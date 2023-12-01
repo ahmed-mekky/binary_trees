04544a08d35a6b60a376fecb0fe401b377a39cc5
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-02 01:54:08 +0200

    binary_trees

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+
+/**
+ * t_sibling - whatever
+ *
+ * @node: ..
+ *
+ * Return: ...
+ */
+binary_tree_t *t_sibling(binary_tree_t *node)
+{
+	if (
+	node == NULL ||
+	node->parent == NULL ||
+	node->parent->left == NULL ||
+	node->parent->right == NULL
+	)
+		return (NULL);
+
+	if (node->parent->left != node)
+		return (node->parent->left);
+
+	return (node->parent->right);
+}
+
+/**
+ * binary_tree_uncle - whatever
+ *
+ * @node: ..
+ *
+ * Return: ...
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL ||)
+		return (NULL);
+
+	return (t_sibling(node->parent));
+}
